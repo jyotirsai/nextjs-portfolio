bb3fcfba18fca3600cde3b92c8b22e63b5e59261
jyotirsai <jyotirsai28@gmail.com>
2021-05-10 23:08:32 -0600

    image update 5

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: 'Gearbox Design',
     description: `Designed a 5-speed manual transmission for a Mechanical Design class in a team of 6.`,
-    imgSrc: '/static/images/gearbox.png',
+    imgSrc: '/static/images/gearbox-render.png',
     href: '/blog/gearbox-design',
   },
   {
     title: 'Aim Duel',
     description: `HTML Canvas game made using JavaScript, Express, socket.io, and Webpack`,
-    imgSrc: '/static/images/aimduel.png',
+    imgSrc: '/static/images/aimduel-mainmenu.png',
     href: '/blog/browser-game',
   },
 ]
